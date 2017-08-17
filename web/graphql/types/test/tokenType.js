e1534f900b9951983fe97718973d129754ed1bfb
Ashok Mohan <ashokmohank1@gmail.com>
2017-08-17 15:08:07 +0530

    connection relay

--- web/graphql/types/test/tokenType.js
+++ web/graphql/types/test/tokenType.js
@@ -50,12 +50,22 @@ export default new GraphQLObjectType({
           type: GraphQLString,
         }
       },
-      resolve: (obj, args, context) => {
+      resolve: (_, args) => connectionFromArray([{
+        username: 'ashok1',
+        hashedPassword: '0f7fe43cfaac65777d826f7121d1aa5b5b355516',
+        _id: '597b0f5c1cf1272aec5e4367',
+        id: 'VXNlcjo=',
+      }], args),
+      /* resolve: (obj, args, context) => {
         console.log(obj.userId);
+        let x = new Promise((resolve, reject) => {
+          userCore.userService.getUserById(obj.userId, cb => resolve(cb));
+        });
+        console.log(JSON.stringify(x));
         return new Promise((resolve, reject) => {
           userCore.userService.getUserById(obj.userId, cb => resolve(cb));
         });
-      },
+      }, */
     },
   }),
   interfaces: () => [NodeInterface],
